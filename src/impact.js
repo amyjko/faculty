3482845323335bf49b6404ae0379d4729b944981
Andy Ko <ajko@uw.edu>
2017-06-15 08:49:10 -0700

    Minor updates to my bio.

--- src/impact.js
+++ src/impact.js
@@ -29,7 +29,7 @@ class Impact extends React.Component {
 				<p><strong>Books</strong>. I synthesize my research and others into books that summarize our best knowledge. I have written short books about <a href="books/design-methods">HCI+Design</a> and <a href="books/cooperative-software-development">Cooperative Software Development</a></p> 
 				<p><strong>K-12 Computing Education Policy</strong>. I <a href="http://blogs.uw.edu/ajko/2017/04/03/a-glimpse-into-state-level-cs-education-policy-implementation/">participate</a> in the development of <a href="http://www.k12.wa.us/ComputerScience/ImplentationPlanning.aspx">Washington State policy on K-12 computing education</a>.</p>
 				<p><strong>Podcasts</strong>. I contribute to podcasts about my expertise; for example, listen to my thoughts on the software industry on the <a target="_blank" href="http://softwareengineeringdaily.com/2016/02/24/academia-to-industry-in-computer-science-with-andy-ko/">on the Software Engineering Daily podcast</a>.</p>
-				<p><strong>Keynotes</strong>. I share larger visions about my work as in my SPLASH 2016 keynote on <a href="https://www.youtube.com/watch?v=TjkzAls5fsI">a human view of programming langauges</a>.</p>
+				<p><strong>Keynotes</strong>. I share larger visions about my work as in my SPLASH 2016 keynote on <a href="https://www.youtube.com/watch?v=TjkzAls5fsI">a human view of programming languages</a>.</p>
 				
 				{impacts}
 
