f1e4b4aa0de093de88da5a92d1207734103b282d
Andy Ko <ajko@uw.edu>
2017-12-10 20:57:52 -0800

    Adjusted contact info.

--- src/contact.js
+++ src/contact.js
@@ -17,13 +17,11 @@ class ContactInfo extends React.Component {
 	render() {
 		return <div className='row'>
 		
-			<div className="col-xs-12 col-sm-3 col-md-3">
+			<div className="col-xs-6 col-sm-3 col-md-3">
 				<img className='img-thumbnail' alt='Photograph of Andrew J. Ko' src='/ajko/images/mug-ajko.jpg'/>
-				<br/>
-				<br/>
 			</div>
 
-			<div className="col-xs-6 col-sm-5 col-md-5">
+			<div className="col-xs-12 col-sm-5 col-md-5">
 				<span className="lead">Andrew J. Ko, Ph.D.</span>
 				<small>
 					<br/>Associate Professor
@@ -31,34 +29,38 @@ class ContactInfo extends React.Component {
 					<br/><a href="http://ischool.uw.edu" target="_blank">The Information School</a>
 					<br/><a href='http://cs.washington.edu' target="_blank">Computer Science & Engr.</a> (adjunct)
 					<br/><a href="http://www.washington.edu" target="_blank">University of Washington</a>
-					<br/>
 					<br/>Chief Scientist + Co-Founder
 					<br/><a href="http://www.answerdash.com" target="_blank">AnswerDash</a>
 				</small>
 			</div>
 
-			<div className="col-xs-4 col-sm-4 col-md-4">
+			<div className="col-xs-6 col-sm-4 col-md-4">
+
+				<span className="lead">&nbsp;</span>
 
 				<small>
 
-					<p>ajko@uw.edu
-						<br/><a href="https://www.google.com/maps/place/Mary+Gates+Hall.+University+of+Washington/@47.6549552,-122.3101087,994m/data=!3m2!1e3!4b1!4m2!3m1!1s0x549014f2a95c2899:0xb2525ccd291489c!6m1!1e1" target="_blank">Mary Gates Hall 015E</a>
-						<br/>Box 352840
-						<br/>Seattle, WA 98195
-	
-					</p>
+					<p><a href="https://twitter.com/andyjko" className="twitter-follow-button" data-show-count="false" ref="follow"></a></p>
+						
+					<p><a target="_blank" href="https://github.com/andyjko"><img style={{height: "1em"}} src="images/GitHub-Mark-120px-plus.png" /> GitHub</a></p>
+
+					<p>ajko@uw.edu</p>
+
+					<p><a href="https://medium.com/bits-and-behavior" target='_blank'>Blog</a> &sdot; <Link to="/cv">CV</Link></p>
+					
+					<p><a href="https://www.google.com/maps/place/Mary+Gates+Hall.+University+of+Washington/@47.6549552,-122.3101087,994m/data=!3m2!1e3!4b1!4m2!3m1!1s0x549014f2a95c2899:0xb2525ccd291489c!6m1!1e1" target="_blank">Mary Gates Hall 015E</a></p>
 					
 					<p>Office hours: Wed 330-430</p>
-				
-				<a href="https://twitter.com/andyjko" className="twitter-follow-button" data-show-count="false" ref="follow"></a>
-				
-				<p><a target="_blank" href="https://github.com/andyjko"><img style={{height: "1em"}} src="images/GitHub-Mark-120px-plus.png" /> GitHub</a></p>
-
-				<p><a href="https://medium.com/bits-and-behavior" target='_blank'>Blog</a> | <Link to="/cv">CV</Link></p>
 
 				</small>
-		
-			</div>		
+				
+			</div>
+			
+			<div className="col-xs-6 col-sm-4 col-md-4">
+				
+
+			</div>				
+	
 		</div>
 	}
 }
