25d1dbc73981a2ea26fb00346ed381c4dca47b60
Andy Ko <ajko@uw.edu>
2017-12-21 08:56:16 -0800

    Added HCID 520.

--- src/teaching.js
+++ src/teaching.js
@@ -5,12 +5,13 @@ class Teaching extends React.Component {
 		return (
 			<div>
 				<div className='lead'>
-					Here are some of the latest courses I've been teaching.
+					Here are some of the courses I teach regularly.
 				</div>
 
 				<ul>
 					<li><a target="_blank" href="info360">INFO 360 Design Methods</a></li>
 					<li><a target="_blank" href="info461">INFO 461 Cooperative Software Development</a></li>
+					<li><a target="_blank" href="hcid520">HCID 520 User Interface Software and Technology</a></li>
 				</ul>
 
 			</div>
