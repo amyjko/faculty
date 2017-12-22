417a312f8e0e8e539e7b8179080c1298d72146f0
Andy Ko <ajko@uw.edu>
2017-12-22 13:08:27 -0800

    Beautified books page.

--- src/books.js
+++ src/books.js
@@ -5,12 +5,27 @@ class Books extends React.Component {
 		return (
 			<div>
 				<div className='lead'>
-					I've written a few accessible online books to support my teaching. Feel free to use them for your classes. These are living documents, so if you see something that I should fix or add, let me know!
+					After my first sabbatical in 2014, I started writing short, accessible online books as an alternative to lectures. Feel free to use them for your classes. These are living documents, so if you see something that I should fix or add, let me know!
 				</div>
 
-				<div className="lead"><a target="_blank" href="/ajko/books/design-methods">Design Methods</a></div>
-				<div className="lead"><a target="_blank" href="/ajko/books/cooperative-software-development">Cooperative Software Development</a></div>
+				<div className="project row">
+					<div className="col-md-3">
+						<img className='img-responsive gap-bottom-right' src="/books/design-methods/images/cover.jpg" style={{width: 140}} />
+					</div>
+					<div className="col-md-9">
+						<div className='lead'><a target="_blank" href="/ajko/books/design-methods">Design Methods</a>. <small>This short book covers interaction design, design methods, and HCI fundamentals. I have my students read it in the first five weeks of class, then launch into a five week design project.</small></div>	
+					</div>
+				</div>
 
+				<div className="project row">
+					<div className="col-md-3">
+						<img className='img-responsive gap-bottom-right' src="/books/cooperative-software-development/images/cover.jpg" style={{width: 140}} />
+					</div>
+					<div className="col-md-9">
+						<div className='lead'><a target="_blank" href="/ajko/books/design-methods">Cooperative Software Development</a>. <small>This short book covers software engineering fundamentals, but from a human, social, collaborative, and organizational perspective, rather than from a technical perspective. </small></div>	
+					</div>
+				</div>
+				
 			</div>
 		);
 	}
