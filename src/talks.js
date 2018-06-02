fd6ef358183490ddc05648eaf9f690f7bf863f55
Andy Ko <ajko@uw.edu>
2018-06-02 07:42:06 -0700

    Added ICSE 2018 invited talks.

--- src/talks.js
+++ src/talks.js
@@ -10,6 +10,24 @@ class Talks extends React.Component {
 
 				<div className="project row">
 					<div className="col-md-3">
+						<a href="https://www.youtube.com/watch?v=lx7g-T10WxQ"><img className='img-responsive gap-bottom-right' src="/ajko/images/icse2018mip.png" style={{width: 140}} /></a>
+					</div>
+					<div className="col-md-9">
+						<div className='lead'><a href="https://www.youtube.com/watch?v=lx7g-T10WxQ">Big Ideas Behind the Whyline</a> (2018). <small>This was an invited talk for my most influential paper award on my dissertation work on the Whyline. I reflect on the ideas, both technical, scientific, and otherwise that have shaped my discoveries. See <a href="slides/ICSE2018MIP.pdf">the slides</a>.</small></div>
+					</div>
+				</div>
+
+				<div className="project row">
+					<div className="col-md-3">
+						<a href="https://youtu.be/Oh5NuyTMB4A"><img className='img-responsive gap-bottom-right' src="/ajko/images/msricpc18.png" style={{width: 140}} /></a>
+					</div>
+					<div className="col-md-9">
+						<div className='lead'><a href="https://youtu.be/Oh5NuyTMB4A">Mining the Mind, Minding the Mine: Grand Challenges in Comprehension and Mining</a> (2018). <small>This was an invited joint keynote at the <a href="https://2018.msrconf.org/">International Conference on Mining Software Repositories</a>, and the <a href="https://conf.researchr.org/home/icpc-2018">International Conference on Program Comprehension</a>. I argue that the two areas need each other, that they both need theory, and that they both need to ask more relevant questions. See <a href="slides/MSRICPC2018Keynote.pdf">the slides</a>.</small></div>
+					</div>
+				</div>
+
+				<div className="project row">
+					<div className="col-md-3">
 						<a href="https://www.youtube.com/watch?v=mkzHIhKaUX4&feature=youtu.be"><img className='img-responsive gap-bottom-right' src="/ajko/images/stanford18.png" style={{width: 140}} /></a>
 					</div>
 					<div className="col-md-9">
