27f4b875d7f0dd35dee6d6090c57be4c286080f1
Andy J. Ko <ajko@uw.edu>
2019-07-24 16:10:30 -0700

    Fixed a few typos in contact.

--- components/contact.js
+++ components/contact.js
@@ -24,7 +24,7 @@ class ContactInfo extends React.Component {
 					alt="A photograph of the front of Mary Gates Hall."
 					link="https://www.google.com/maps/place/Mary+Gates+Hall.+University+of+Washington/@47.6549552,-122.3101087,994m/data=!3m2!1e3!4b1!4m2!3m1!1s0x549014f2a95c2899:0xb2525ccd291489c!6m1!1e1"
 					header="Mary Gates Hall 015E"
-					content="My office is in a locked suite on the ground floor of Mary Gates. If you knock, a friendly student will let you in, unless they all have their noise-canceling headphones on. My office hours are currently by appointment, but you can always stop by to see if my door is open, which means I'm available to chat."
+					content="My office is in a locked suite on the ground floor of Mary Gates. If you knock, a friendly student will let you in (unless they all have their noise-canceling headphones on!) My office hours are currently by appointment, but you can always stop by to see if my door is open, which means I'm available to chat."
 				/>
 
 				<Block 
@@ -32,7 +32,7 @@ class ContactInfo extends React.Component {
 					alt="The Slack logo."
 					link="https://uwdub.slack.com/"
 					header="Slack"
-					content="I use Slack for all of my internal lab communications, and for many of my internal UW communication. I treat Slack like instant messaging, and after hours, like email. If you're at UW, join DUB Slack and DM me. I'm usually pretty responsive throughout the day."
+					content="I use Slack for all of my internal lab communications, and for much of my UW communication with colleagues and PhD students. During the work day I treat Slack like instant messaging, and after hours I treat it like email, reading it in the morning. If you're at UW, join DUB Slack and DM me. I'm usually pretty responsive throughout the day."
 				/>
 
 				<Block 
@@ -56,7 +56,7 @@ class ContactInfo extends React.Component {
 					alt="The LinkedIn logo."
 					link="https://www.linkedin.com/in/ajko/"
 					header="LinkedIn"
-					content="I use LinkedIn as my primary contact manager. I usually only accept connections from people I've met IRL and have a professional relationship with (students, collaborators, professional colleagues). I do read my LinkedIn messages as part of my morning email routine."
+					content="I use LinkedIn as my primary contact manager. I usually only accept connections from people I've met IRL and have a professional relationship with (students, collaborators, professional colleagues). I read my LinkedIn messages as part of my morning email routine."
 				/>
 
 				<Block 
