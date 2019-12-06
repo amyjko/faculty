711ffaccb407c73f6597e7d973f5dff2b55294b3
Andy J. Ko <ajko@uw.edu>
2019-12-06 08:46:05 -0800

    Sorted classes.

--- components/teaching.js
+++ components/teaching.js
@@ -10,6 +10,17 @@ class Teaching extends React.Component {
 				</div>
 
 				<Block
+					image={this.props.app.getWebRoot() + "/images/course-info200.png"}
+					alt="A black and white version of an old painting with a child reading a newspaper"
+					link="https://canvas.uw.edu/courses/1223117"
+					header="INFO 200 Intellectual Foundations of Informatics"
+					content=
+						<span>
+							. This survey course covers big ideas in information, information technology, information systems, and human-computer interaction. Students learn the history of information, foundational concepts in information, the relationship between information and computing, skills in analyzing and designing information systems, and the role of information systems in society. See <a href="https://medium.com/bits-and-behavior/big-ideas-about-information-c09e4d9c60bd">my Medium post on the big ideas in the course</a>.
+						</span>
+				/>
+
+				<Block
 					image={this.props.app.getWebRoot() + "/images/course-info360.png"}
 					alt="A line drawing of two people discussing a sketch"
 					link = "courses/info360/"
@@ -34,17 +45,6 @@ class Teaching extends React.Component {
 					link="https://canvas.uw.edu/courses/1256337"
 					header="HCID 520 User Interface Software and Technology"
 					content=". This graduate course teaches students they history, theory, and foundations of user interfaces. Students acquire a literacy for communicating and reasoning about interfaces."
-				/>
-
-				<Block
-					image={this.props.app.getWebRoot() + "/images/course-info200.png"}
-					alt="A black and white version of an old painting with a child reading a newspaper"
-					link="https://canvas.uw.edu/courses/1223117"
-					header="INFO 200 Intellectual Foundations of Informatics"
-					content=
-						<span>
-							. This survey course covers big ideas in information, information technology, information systems, and human-computer interaction. Students learn the history of information, foundational concepts in information, the relationship between information and computing, skills in analyzing and designing information systems, and the role of information systems in society. See <a href="https://medium.com/bits-and-behavior/big-ideas-about-information-c09e4d9c60bd">my Medium post on the big ideas in the course</a>.
-						</span>
 				/>
 
 			</div>
