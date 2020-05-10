fe00e305f5c05aec3ec9133ae4f669f72645dadb
Andy J. Ko <ajko@uw.edu>
2020-05-10 14:31:12 -0700

    Fixed #37, adding hand drawn images.

--- components/footer.js
+++ components/footer.js
@@ -9,18 +9,7 @@ class Footer extends React.Component {
 			<div>
 				<hr/>
 				<small>
-					Copyright Amy J. Ko.
-					<br/>See <a href="https://github.com/amyjko/faculty" target="_blank">this site's GitHub repository</a> to view source and provide feedback.
-					<br/>Computing diversity and equity icon "Diversity" by Stephen Plaster from the Noun Project. 
-					Understanding program behavior icon "Dependency" by Knut M. Synstad from the Noun Project.
-					Programming problem solving icon "Thinking" icon by AomAm from the Noun Project.
-					Software engineering expertise icon "Teamwork" by Maxim Kulikov from the Noun Project.
-					API learning icon "Shape" by Noura Mbarki from the Noun Project.
-					Programming language learning icon "Code" by faisalovers from the Noun Project.
-					Machine learning literacy icon "Data" by priyanka from the Noun Project.
-					Bug triage icon "feedback" by Adrien Coquet from the Noun Project.
-					Methods and theory icon "atom" by Chattapat Kunlapatpisit from the Noun Project.
-					User interface mining icon "Wireframe" by mikicon from the Noun Project.
+					Copyright Amy J. Ko. See <a href="https://github.com/amyjko/faculty" target="_blank">this site's GitHub repository</a> to view source and provide feedback.
 				</small>
 			</div>
 	    )
