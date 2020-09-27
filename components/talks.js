178c0bd463cba981d25c734c4ced387aa0f8ded9
Amy J. Ko <ajko@uw.edu>
2020-09-27 14:57:29 -0700

    Fixed #69, adding blog posts to talks.

--- components/talks.js
+++ components/talks.js
@@ -8,29 +8,43 @@ class Talks extends React.Component {
 		
 		var blocks = _.map(this.props.app.getProfile().getTalks(), (talk, index) => {
 			
-				return <Block
-					key={"talk" + index}
-					image={talk.image ? this.props.app.getWebRoot() + "/images/talks/" + talk.image: null}
-					alt={talk.alt}
-					link={talk.recording ? talk.recording : talk.practice ? talk.practice : talk.slides }
-					header={talk.title}
-					content={
-						<span>
-							&nbsp; { talk.keynote ? <span className="award">&#x2605; Keynote</span> : null }
-							<br/><small><em>{talk.url ? <a href={talk.url}>{talk.venue}</a> : talk.venue}</em></small>
-							<br/><small>{talk.date}</small>
-							<br/>{talk.description} 
-							<br/>
-							<small>
-								{talk.recording ? <a href={talk.recording}>Recording</a> : null } 
-								{talk.recording ? <span>&nbsp;&sdot;&nbsp;</span> : null }
-								{talk.practice ? <a href={talk.practice}>Rehearsal</a> : null } 
-								{talk.practice ? <span>&nbsp;&sdot;&nbsp;</span> : null }
-								{talk.slides ? <a href={talk.slides}>Slides</a> : null } 
-							</small>
-						</span>
-					}
-				/>
+			var links = [];
+
+			if(talk.recording)
+				links.push(<a key="talk" href={talk.recording} target="_blank">Recording</a>);
+			if(talk.practice) {
+				if(links.length > 0)
+					links.push(<span key="talk-dot"> &sdot; </span>);
+				links.push(<a href={talk.practice} target="_blank">Rehearsal</a>);
+			}
+			if(talk.slides) {
+				if(links.length > 0)
+					links.push(<span key="slides-dot"> &sdot; </span>);
+				links.push(<a href={talk.slides} target="_blank">Slides</a>);
+			}
+			if(talk.blog) {
+				if(links.length > 0)
+					links.push(<span key="talk-dot"> &sdot; </span>);
+				links.push(<a href={talk.blog} target="_blank">Blog post</a>);
+			}
+
+			return <Block
+				key={"talk" + index}
+				image={talk.image ? this.props.app.getWebRoot() + "/images/talks/" + talk.image: null}
+				alt={talk.alt}
+				link={talk.recording ? talk.recording : talk.practice ? talk.practice : talk.slides }
+				header={talk.title}
+				content={
+					<span>
+						&nbsp; { talk.keynote ? <span className="award">&#x2605; Keynote</span> : null }
+						<br/><small><em>{talk.url ? <a href={talk.url}>{talk.venue}</a> : talk.venue}</em></small>
+						<br/><small>{talk.date}</small>
+						<br/><small>{talk.description}</small>
+						<br/>
+						<div className="border-top"><small>{links}</small></div>
+					</span>
+				}
+			/>
 		})
 		
 		return (
