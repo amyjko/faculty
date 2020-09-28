e662c170273fe1db5d6f8c648bb78505440c3f05
Amy J. Ko <ajko@uw.edu>
2020-09-27 20:37:24 -0700

    Fixed #81, implementing filters as ARIA radiobutton role

--- components/facets.js
+++ components/facets.js
@@ -29,11 +29,15 @@ class Facets extends React.Component {
             <div>
             {
                 _.map(Object.keys(this.props.facets), facet =>
-                    <p key={"facet-" + facet}>
+                    <p key={"facet-" + facet} role="radiogroup" aria-label="Filter">
                     {
                         _.map(this.props.facets[facet].sort(), value =>
                             <mark 
                                 key={"topic-" + value} 
+                                role="radio"
+                                aria-checked={facet in this.state.selection && this.state.selection[facet] === value}
+                                tabindex={facet in this.state.selection && this.state.selection[facet] === value ? 0 : 1}
+                                aria-label={"Filter by " + value}
                                 className={"clickable topic" + (facet in this.state.selection && this.state.selection[facet] === value ? " selected" : "")} 
                                 onClick={this.selectValue.bind(this, facet, value)}
                             >
