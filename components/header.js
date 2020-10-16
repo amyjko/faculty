cfa5104353db8b064e640def23b2a1778585861a
Amy J. Ko <ajko@uw.edu>
2020-10-16 11:42:46 -0700

    Consistent name formatting as header.

--- components/header.js
+++ components/header.js
@@ -17,9 +17,10 @@ class Header extends React.Component {
 					</div>
 		
 					<div className="col-xs-9 col-sm-9 col-md-9" style={{lineHeight: "1.25em"}}>
-						<div className="lead" style={{marginBottom: 0}}>Amy J. Ko, Ph.D. <small className="hidden-xs"><small><em><a href="https://www.mypronouns.org/she-her">she/her/hers</a></em></small></small></div>
+						<h2 style={{margin: 0}}>Amy J. Ko <small>Ph.D.</small></h2>
 						<small>
- 							<a href="https://ap.washington.edu/ahr/academic-titles-ranks/professor/" target="_blank">Professor</a><span className="hidden-xs"> &sdot; <a href="https://ischool.uw.edu/academics/informatics">Informatics</a> <a href="https://ischool.uw.edu/about/leadership/committees" target="_blank">Program Chair</a></span>
+							<a href="https://www.mypronouns.org/she-her">she/her/hers</a>
+ 							<br/><a href="https://ap.washington.edu/ahr/academic-titles-ranks/professor/" target="_blank">Professor</a><span className="hidden-xs"> &sdot; <a href="https://ischool.uw.edu/academics/informatics">Informatics</a> <a href="https://ischool.uw.edu/about/leadership/committees" target="_blank">Program Chair</a></span>
 							<br/><a href="http://ischool.uw.edu" target="_blank">The Information School</a>
 							<span className="hidden-xs"><br/><a href="http://cs.uw.edu" target="_blank">Paul G. Allen School of Computer Science &amp; Engineering (courtesy)</a></span>
 							<br/><a href="http://www.washington.edu" target="_blank">University of Washington<span className="hidden-xs">, Seattle</span></a>
