3de4d2f195acecb45e4a4caa08cbfc939b52b757
Amy J. Ko <ajko@uw.edu>
2021-07-19 17:55:53 -0700

    Revised lablets.

--- components/lablets.js
+++ components/lablets.js
@@ -9,13 +9,13 @@ class Lablets extends React.Component {
 					Are you an undergraduate at UW that wants to join the Code and Cognition lab? Read on for details about our experimental <em>lablets</em> model.
 				</div>
 
-				<p className="alert alert-danger">This is not yet final. However, if you read this and have feedback, do send it! We hope to launch it in Autumn 2021.</p>
+				<p className="alert alert-danger">This is not yet final. However, if you read this and have feedback, <a href="mailto:ajko@uw.edu">email Amy</a>! We hope to recruit in Autumn 2021 and launch it in Winter 2022.</p>
 
 				<p>
-					Every year, I have dozens of students reach out to participate in the lab, but my doctoral students and I only have capacity to meaningfully mentor a few students at a time.
+					Every year, dozens of students reach out to my lab to participate, but we don't have the capacity to meaningfully mentor everyone who wants to join.
 					To offer more students the opportunity to participate in research while respecting our limited attention, we're trying a new <strong>lablets</strong> model.
-					This idea has three core principles:
 				</p>
+
 
 				<ul>
 					<li><em>Help students develop interest in CS education research and teaching.</em> If you join a lablet, we commit to helping you develop your interests.</li>
@@ -23,69 +23,121 @@ class Lablets extends React.Component {
 					<li><em>Ensure any barriers we create are beneficial.</em> We'll have to create some barriers to avoid overwhelming Amy and her doctoral students, but any barriers do create should be helpful in some way.</li>
 				</ul>
 
-				In the spirit of those principles, here's how lablets work:
-
 				<h3>What is a lablet?</h3>
-				<p>					
-					During any given academic quarter, our main lab will run as normal.
-					This includes once or twice weekly between Amy, her Ph.D. students, and any other research collaborators.
-					This is intentionally a small group, which keeps the core culture of the lab intact.
-				</p>
-				
-				<p>				 
-					However, in addition to the lab, there will also be one or more lablets running in parallel to the lab.
+
+				<p>
+					A lablet is:
 				</p>
 
 				<ul>
-					<li>
-						Lablets should be fewer than 8 people. We encourage the creation of new ones each quarter if a lablet gets too big.
-						</li>
-					<li>
-						Each lablet has an undergraduate <strong>leader</strong>, elected by majority by the members of the lablet at the beginning of each quarter, and approved by Amy.
-						Their job is to host weekly meetings, facilitate lablet activities, and communicate regularly with lablet members.
-						Lablet leaders are eligible to join the main lab meetings.
-					</li>
-					<li>
-						Each lablet also has a doctoral student <strong>supervisor</strong>, whose responsibility it is to check in weekly with the lablet leader and participate in lablet meetings.
-					</li>
+					<li>A group of undergraduates who self-organize around a common interest, that...</li>
+					<li>Meets weekly (or more frequently) to do research activities related to the focus of the Code &amp; Cognition lab, and that...</li>
+					<li>Starts and ends on a quarterly basis, and that...</li>
+					<li>Has 8 or fewer people, and is...</li>
+					<li>Led by an undergraduate leader elected by majority by the members of the lablet at the beginning of each quarter, and is...</li>
+					<li>Supervised by either Amy or one of her postdocs or doctoral students.</li>
 				</ul>
 
 				<p>
-					Lablet activities can include a variety of things, at the discussion of the leader, supervisor, and group.
-					These can include weekly meetings, reading groups, critiques, brainstorms, activism, tutorials, planning research, and conducting research.
-					It's up to the leader and supervisor to ensure meaningful activities.
-					We'll provide meeting templates for all of the above activities to help you understand roles and expectations.
+					Lablets run separately from the main lab, but engage in similar activities, including weekly meetings, reading groups, critiques, brainstorms, activism, tutorials, planning research, and conducting research.
+					It's up to the leader and supervisor to ensure meaningful activities, maintaining a weekly agenda.
+					We'll provide meeting templates (TBD) for all of the above activities to help you understand roles and expectations.
 					However, you're always welcome to deviate from them, or suggest improvements to them.
-					All lablet members are welcome to post in #codeandcognitionlab regardless of their status.
-					Lablet leaders are required to post a weekly progress report about lablet activities in #codeandcognition to help other lab and lablet members identify opportunities to talk.
 				</p>
 
-				<h3>How do I join a lablet?</h3>
+				<h3>Why would I want to join a lablet?</h3>
+
 				<p>
-					You can see active lablets on this spreadsheet, with their focus, membership, and leadership.
-					Anyone can join a lablet by filling out this form.
-					To fill out the form, you'll have to read a paper from the reading list and indicate what interested you about it.
-					You'll also have to make a pledge to Amy with the amount of time you commit for the coming quarter.
-					After you submit, Amy will receive a notification, then add your information to the lablets spreadsheet, and notify the lablet leader and supervisor.					
+					There are many reasons!
 				</p>
 
-				<h3>How do we do research with you from a lablet?</h3>
+				<ul>
+					<li><em>To learn about research</em>. By reading, talking about, and doing research, you'll learn the difference between research and practice.</li>
+					<li><em>To find students with shared interests</em>. Lablets bringing together students with shared interests.</li>
+					<li><em>To join the Code &amp; Cognition lab</em>. If you join a lablet that successfully proposes a research project, you'll be invited to join the main lab meetings and get advising from Amy and other lab members.</li>
+					<li><em>To prepare for doctoral studies</em>. Doing research is the best way to prepare to apply to Ph.D. programs.</li>
+				</ul>
+
+				<p>Lablet members may:</p>
+
+				<ul>
+					<li>Participate in all lablet activities.</li>
+					<li>Post in ComputingEd@UW Slack #codeandcognitionlab</li>
+					<li>Earn 1 credit by registering for INFO 499 Independent Study with Amy (TBD). (Earning this credit only requires a short end of quarter reflection).</li>
+					<li>Meet graduation requirements, such as research capstones or other requirements related to research.</li>
+					<li>In certain cases, earn hourly or salaried pay, if funding is available.</li>
+				</ul>
+
+				<p>To encourage focus, you can only be part of one lablet at a time.</p>
+
+				<h3>What does a lablet leader have to do?</h3>
+
+				<p>
+					Lablet leaders must:
+				</p>
+
+				<ul>
+					<li>Host weekly meetings, facilitate lablet activities, and communicate regularly with lablet members.</li>
+					<li>Talk regularly with the lablet supervisor to brainstorm activities.</li>
+					<li>Set a quarterly goal when forming the lablet.</li>
+					<li>Post a weekly progress report about lablet activities in #codeandcognition. This helps other lab and lablet members identify opportunities to talk.</li>
+				</ul>
+
+				<p>Lablet leaders may:</p>
+
+				<ul>
+					<li>Join the main lab meetings.</li>
+					<li>Propose research projects to the lab.</li>
+				</ul>
+
+				<h3>What does a lablet supervisor do?</h3>
+
+				<p>
+					Lablet supervisors must:
+				</p>
+
+				<ul>
+					<li>Be a postdoc, doctoral student, or Amy</li>
+					<li>Check in weekly with the lablet leader</li>
+					<li>Participate in lablet meetings</li>
+					<li>Meet with Amy before each quarter to set expectations.</li>
+				</ul>
+
+				<h3>How do I create a lablet?</h3>
+
+				<p>
+					Follow these steps:
+				</p>
+
+				<ol>
+					<li>First check the active lablets spreadsheet (TBD) to ensure there isn't a suitable lablet to join already.</li>
+					<li>If you see a need for a new lablet, read the responsibilities above to ensure you are willing to commit to the job.</li>
+					<li>If you're willing, fill out this form to propose a lablet (TBD).</li>
+					<li>If the lablet is approved by Amy, she will introduce you to your supervisor, who will share an onboarding.</li>
+					<li>Run your lablet!</li>
+				</ol>
+
+				<h3>How do I join an existing lablet?</h3>
+
+				<p>
+					Follow these steps:
+				</p>
+
+				<ol>
+					<li>Review the active lablets (TBD).</li>
+					<li>Fill out this form (TBD).</li>
+					<li>After you submit, Amy will receive a notification, and review your application.</li>
+					<li>If you're approved, add your information to the lablets spreadsheet, and notify the lablet leader and supervisor.</li>
+				</ol>
+
+				<h3>How does our lablet propose a research project?</h3>
 				<p>
 					Any lablet can conduct research without anyone's permission.
-					However, if you want the help of a Amy or doctoral student on a project, you'll need to write a project proposal.
-					We'll offer a proposal template to help you structure your proposal.
+					However, if you want the help of a Amy or oher postdocs or doctoral students, you'll need to write a project proposal using this template (TBD).
 					You can submit a proposal at any time to your lablet supervisor, who will offer you feedback.
-					Your lablet might have to iterate several times before it's approved; we might even reject it if it's not a well formed project.
-					If it's accepted, however, you'll earn your Amy and your supervisor's full attention to bring your project to completion.
+					Your lablet might have to iterate several times before it's approved; we might even reject it if it's not a well-formed project.
+					If it's accepted, however, you'll earn your Amy and your supervisor's attention to bring your project to completion.
 					Everyone who was included on the proposal is welcome to join the main lab meetings to discuss research.
-				</p>
-
-				<h3>Can we get credits or pay?</h3>
-				<p>
-					There are many ways to account for your time. 
-					You can request independent study credit with Amy.
-					If you're leading a research project, you can request hourly compensation, subject to availability.
-					You can request capstone or thesis credit that aligns with your graduation requirements.
 				</p>
 
 				<h3>How do I leave a lablet or move to a new one?</h3>
@@ -96,7 +148,7 @@ class Lablets extends React.Component {
 					If everyone leaves the lablet, then the lablet is over.
 				</p>
 
-				<h3>How do you monitor lablets?</h3>
+				<h3>How do you monitor lablet activity?</h3>
 				<p>
 					Supervisors will conduct a quarterly lablet health check, sending a report to Amy each quarter
 					The report will comment on lablet engagement, clarity of focus, and confidence in lablet leadership.
