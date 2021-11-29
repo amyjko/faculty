24b5df0c99c46540839dc8d2e4d855c37738c601
Amy J. Ko <ajko@uw.edu>
2021-11-28 20:43:18 -0800

    Fixed typos.

--- components/bio.js
+++ components/bio.js
@@ -41,10 +41,10 @@ class Biography extends React.Component {
 					She surrounds herself with similarly curious people, particularly students, and they wander about asking questions about the surprising ways that code has woven itself through people's lives.
 					She started off inventing ways of making programming easier, but then got disillusioned with productivity.
 					She then got excited about the ways that programmers interact with massive communities of people using their code, and that somehow turned into a startup that sold help tools.
-					After she returned from her icky capitalist stint as a CTO, she decided to focus on making people smarter instead of tools smarter, and began studying how to making learning to code easier.
+					After she returned from her icky capitalist stint as a CTO, she decided to focus on making people smarter instead of tools smarter, and began studying how to make learning to code easier.
 					Now she mostly thinks about how we can teach people to write code that's not so terribly oppressive to people at the margins.
-					Somewhere along the way, she published a punch of papers and won a bunch of awards, which mostly means that people often think what she had to say is interesting.
-					She's grateful to <a href="http://www.cmu.edu">Carnegie Mellon University</a> and <a href="http://www.oregonstate.edu">Oregon State University</a> for being mostly safe spaces to bumble through the world's endless expanse of mysteries, and to the many students and taxpayers that pay her to keep doing it.
+					Somewhere along the way, she published a punch of papers and won a bunch of awards, which mostly means that people think her discoveries and insights are neat.
+					She's grateful to <a href="http://www.cmu.edu">Carnegie Mellon University</a> and <a href="http://www.oregonstate.edu">Oregon State University</a> for being mostly safe spaces to bumble through the world's endless expanse of mysteries as a student, and to the many students and taxpayers that pay her to keep doing it as a professor.
 				</p>
 
 				<Linkable app={this.props.app} id="age">Age</Linkable>
