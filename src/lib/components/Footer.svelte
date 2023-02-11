22c4926ad1c4a8f014bb54f362e181fd476cb8ba
Amy J. Ko <ajko@uw.edu>
2023-02-11 09:03:17 -0800

    Clarified copyright in footer.

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,13 +1,50 @@
 <script>
+    // @ts-nocheck
+
     import External from './External.svelte';
 </script>
 
 <div>
     <hr />
     <small>
-        Copyright Amy J. Ko. See <External
-            to="https://github.com/amyjko/faculty"
-            >this site's GitHub repository</External
-        > to view source and provide feedback.
+        <p
+            xmlns:dct="http://purl.org/dc/terms/"
+            xmlns:vcard="http://www.w3.org/2001/vcard-rdf/3.0#"
+        >
+            <a
+                rel="license"
+                href="http://creativecommons.org/publicdomain/zero/1.0/"
+            >
+                <img
+                    src="http://i.creativecommons.org/p/zero/1.0/88x31.png"
+                    style="border-style: none;"
+                    alt="CC0"
+                />
+            </a>
+            To the extent possible under law,
+            <a rel="dct:publisher" href="https://faculty.washington.edu/ajko">
+                <span property="dct:title">Amy J. Ko</span></a
+            >
+            has waived all copyright and related or neighboring rights to the design
+            and implementation of
+            <span property="dct:title">Amy's faculty site</span>. This work is
+            published from the
+            <span
+                property="vcard:Country"
+                datatype="dct:ISO3166"
+                content="US"
+                about="https://faculty.washington.edu/ajko"
+            >
+                United States</span
+            >. See <External to="https://github.com/amyjko/faculty"
+                >this site's GitHub repository</External
+            > to view source and provide feedback.
+        </p>
     </small>
 </div>
+
+<style>
+    img {
+        height: 1em;
+    }
+</style>
