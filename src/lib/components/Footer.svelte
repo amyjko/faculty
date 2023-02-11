1dfd5eb12ba8e7d634cea4411749eeee8c247bc6
Amy J. Ko <ajko@uw.edu>
2023-02-11 09:24:32 -0800

    Fixed protocols on copyright links.

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -8,15 +8,15 @@
     <hr />
     <small>
         <p
-            xmlns:dct="http://purl.org/dc/terms/"
-            xmlns:vcard="http://www.w3.org/2001/vcard-rdf/3.0#"
+            xmlns:dct="https://purl.org/dc/terms/"
+            xmlns:vcard="https://www.w3.org/2001/vcard-rdf/3.0#"
         >
             <a
                 rel="license"
-                href="http://creativecommons.org/publicdomain/zero/1.0/"
+                href="https://creativecommons.org/publicdomain/zero/1.0/"
             >
                 <img
-                    src="http://i.creativecommons.org/p/zero/1.0/88x31.png"
+                    src="https://i.creativecommons.org/p/zero/1.0/88x31.png"
                     style="border-style: none;"
                     alt="CC0"
                 />
