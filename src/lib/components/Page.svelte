f99486bfbba43800228632a232b571669aa84c32
Amy J. Ko <ajko@uw.edu>
2023-02-11 08:57:39 -0800

    Fixed error page layout.

---
+++ src/lib/components/Page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import Header from '$lib/components/Header.svelte';
+    import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<div class="page">
+    <div class="header"><Header /></div>
+    <div class="content">
+        <slot />
+        <Footer />
+    </div>
+</div>
+
+<style>
+    /* Small */
+    @media only screen and (max-width: 800px) {
+        :global(body) {
+            margin: 1em;
+        }
+
+        .page {
+            display: block;
+        }
+    }
+
+    /* Large */
+    @media only screen and (min-width: 800px) {
+        :global(body) {
+            margin: auto;
+            max-width: 80em;
+        }
+
+        .page {
+            margin: 3em;
+            display: flex;
+            flex-direction: row;
+        }
+
+        .header {
+            margin-right: var(--margin);
+            flex: 0 0 15em;
+        }
+
+        .content {
+            margin-top: 10em;
+            max-width: 40em;
+        }
+    }
+</style>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,14 @@
-<script>
+<script lang="ts">
+    import Page from '$lib/components/Page.svelte';
     import Alert from '$lib/components/Alert.svelte';
 </script>
 
-<Alert>Saddest of pandas, this information doesn't seem to exist.</Alert>
+<Page>
+    <h1> Uh oh. </h1>
+
+    <Alert
+        >This page doesn't seem to exist. If you think it's supposed to or it
+        used to exist, feel free to <a href="mailto:ajko@uw.edu">write me</a> and
+        I'll see if I broke something.</Alert
+    ></Page
+>
