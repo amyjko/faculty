801f97f2dbc89d7d126dbc6c073e7385257606c9
Amy J. Ko <ajko@uw.edu>
2023-02-20 07:18:19 -0800

    Added link to Bookish.

--- src/routes/(app)/books/+page.svelte
+++ src/routes/(app)/books/+page.svelte
@@ -2,13 +2,17 @@
     import Block from '$lib/components/Block.svelte';
     import Image from '$lib/components/Thumbnail.svelte';
     import { profile } from '$lib/models/stores';
+    import Link from '$lib/components/Link.svelte';
 </script>
 
 <h1> These are books I've authored. </h1>
 
 <p>
     Unlike print books, they are digital, searchable, interactive, and web
-    accessible. They are also living documents: I update them regularly with new
+    accessible. I write them on <Link to="https://bookish.press"
+        >Bookish.press</Link
+    >, a platform I created to write simple, beautiful, accessible web-based
+    books. The books are also living documents: I update them regularly with new
     ideas, citations, and chapters, especially in response to feedback.
 </p>
 
