4122071383c1449c19ef7fd593895a9d7565ee46
Amy J. Ko <ajko@uw.edu>
2023-09-01 09:19:43 -0700

    Fixed static site generation.

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
         adapter: adapter({
             pages: 'build',
             assets: 'build',
-            fallback: 'index.html',
+            fallback: undefined,
         }),
         paths: {
             base: '/ajko',
