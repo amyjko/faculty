17b188932ddb0ad57de8d25cedeadece5e47d3e4
Amy J. Ko <ajko@uw.edu>
2023-12-15 12:33:36 -0800

    Removed redundant content from wordplaypen page.

--- src/routes/(app)/wordplaypen/+page.svelte
+++ src/routes/(app)/wordplaypen/+page.svelte
@@ -163,146 +163,32 @@
     suffice.</p
 >
 
-<p>Participation is quarterly and at a minimum will involve: </p>
-
-<ul>
-    <li><strong>Onboarding</strong>, if you haven't yet done it.</li>
-    <li
-        ><strong>A weekly sprint planning meeting</strong> with Amy to build
-        community, identify and assign issues, and offer guidance. The first
-        meeting we will identify issues in
-        <External to="https://github.com/amyjko/wordplay/issues"
-            >Wordplay's GitHub repository</External
-        > that are suitable for your time commitment and skills.</li
-    >
-    <li
-        ><strong>Asynchronous collaboration</strong> on Slack and GitHub on issues.</li
-    >
-    <li
-        >An end of quarter <strong>celebration</strong> of everyone's contributions!</li
-    >
-</ul>
-
 <p
-    >More credits just means more commitments, so sign up for how much time you
-    want to commit. Your INFO 499 grade can be graded or credit/no credit; your
-    final assessment will be based on meeting the milestones you set in the
-    first week of the quarter, but mostly as a disincentive to overcommit.</p
->
-
-<p
-    >I do welcome students on a <strong>volunteer</strong> basis if they can make
-    a time commitment, and demonstrate that they actually have capacity to follow
-    through with it. It doesn't help the project to promise contributions that you
-    can't keep.</p
+    >Participation is quarterly. If you want to participate again, you'll need
+    to register again, or continue as a volunteer. You may volunteer if you can
+    make a time commitment, and demonstrate that they actually have capacity to
+    follow through with it. It doesn't help the project to promise contributions
+    that you can't keep.</p
 >
 
 <Linkable id="expectations">What will I be expected to do?</Linkable>
 
-<p>
-    Independent studies are _independent_: that generally means that much of
-    this quarter will be you, working with others, with minimal guidance. Of
-    course, I will provide much guidance. I am the lead on this project, after
-    all. But my expectation is that you work independently from until you can't,
-    and that you depend on others before you depend on me.
-</p>
-
-<p>
-    For example, if you don't understand some aspect of the system, first read
-    the documentation, if there is any. If there isn't any, ask someone else in
-    the #wordplay channel of our Discord. If they don't know, then you should
-    probably tag me so I can answer. That's what I mean by "independent".
-</p>
-
 <p
-    >In terms of roles, you'll be expected to take one or more of the following
-    roles:</p
+    >See the Wordplaypen wiki on the project's GitHub page for <External
+        to="https://github.com/wordplaydev/wordplay/wiki/wordplaypen"
+    />expectations.</p
 >
-
-<ul>
-    <li
-        ><strong>Engineering</strong>. Choose an issue ready for fixing or
-        buliding, and build it.</li
-    >
-    <li
-        ><strong>Managing</strong>. Make sure everyone has the resources and
-        information they need to excel.</li
-    >
-    <li
-        ><strong>Design</strong>. Envision what we will make in collaboration
-        with who will make it.</li
-    >
-    <li
-        ><strong>Localization</strong>. Improve existing locales and write new
-        ones. Adadpt existing languages for new regions. Build communities of
-        students working on the same language to divide up the localization work
-        while maintaiing a shared voice. Manage out of date translations.</li
-    >
-    <li
-        ><strong>Verification</strong>. Manually test and learn to write
-        automated tests.</li
-    >
-    <li
-        ><strong>Curation</strong>. Write Wordplay examples that help others
-        learn the language and find inspiration. Moderate content and online
-        community.</li
-    >
-</ul>
-
-<p>
-    Remember: you could do more than one of the above. Your quarter could be a
-    bit of localization, some testing, or maybe one big design project plus some
-    project management. There's no reason to limit yourself to a single role if
-    you want to try multiple. Just make sure the work you commit to is feasible
-    for the time you've committed, and that you actually follow through on you
-    work.
-</p>
-
-<Linkable id="schedule">What's the schedule for the quarter?</Linkable>
-
-Because this is structured as a quarterly independent study, there's a certain
-rhythm to our work:
-
-<ul>
-    <li
-        ><strong>Week 1: Onboarding</strong>. If you're new to the project,
-        learn and read everything you need to know to contribute. If you've
-        already contributed in one quarter, you'll help train. By the end of the
-        first week, everyone should have high level knowledge of everything in
-        this document and of the platform's design and implementation.</li
-    >
-    <li
-        ><strong>Week 2: Assignments</strong>. By the end of this week, everyone
-        should choose one or more roles, and begin their role (e.g., engineering
-        and design roles should choose some issues to work on, project managers
-        should choose a set of issues to improve and begin coordinating work on
-        Slack). The goal is to pick enough work that it fills the five weeks,
-        but not so much that you won't finish it. We'll create a milestone for
-        all selected issues.</li
-    >
-    <li
-        ><strong>Weeks 3-9: Work</strong>. Throughout the quarter, work will
-        complete at different rates, and we will release continuously, as fixes
-        and features are ready.</li
-    >
-    <li
-        ><strong>Week 10: Celebrate</strong>. In the last week of the quarter,
-        we celebrate everyone's work by having everyone describe or demo what
-        they accomplished. Your presentation is how you earn credit.</li
-    >
-</ul>
-
-After the quarter, maybe you'll do it all again the next quarter! In summer,
-I'll recruit a couple students to work full time on bigger features that take
-more time.
 
 <Linkable id="otherquestiosn">I have more questions!</Linkable>
 
 <p
-    >First, ensure your question isn't answered on this page. Then, find me on
-    Wordplay's <External to="https://discord.gg/Jh2Qq9husy">Discord</External> and
-    DM me (do not write me an email). I know it's scary to write professors! But
-    I'm friendly, I promise :) Include your name and student status at UW (or set
-    up your Discord server profile with this information), so I have context for
-    who you are.</p
+    >First, ensure your question isn't answered on this page or the <External
+        to="https://github.com/wordplaydev/wordplay/wiki/wordplaypen"
+        >Wordplaypen onboarding</External
+    > page. Then, find me on Wordplay's <External
+        to="https://discord.gg/Jh2Qq9husy">Discord</External
+    > and DM me (do not write me an email). I know it's scary to write professors!
+    But I'm friendly, I promise :) Include your name and student status at UW (or
+    set up your Discord server profile with this information), so I have context
+    for who you are.</p
 >
