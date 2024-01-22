4a5d209a5c5c933161078be9a5458eba0af6930f
Kyle Niemeyer <kyleniemeyer@fastmail.com>
2024-01-21 16:46:03 -0800

    Removes semicolon after year in open-ended commitments

--- src/routes/(app)/commitments/DateRange.svelte
+++ src/routes/(app)/commitments/DateRange.svelte
@@ -25,13 +25,13 @@
     <span
         ><em>{Math.round((10 * range) / yearInMilliseconds) / 10} years</em><br
         />starting <em>{toDateString(start)}</em></span
-    >;
+    >
     <!-- If it's longer than a week, do weeks -->
 {:else if range >= weekInMilliseconds}
     <span
         ><em>{Math.round(range / weekInMilliseconds)} weeks</em><br />starting
         <em>{toDateString(start)}</em></span
-    >;
+    >
 {:else}
     <!-- If it's less than a week, just do the dates -->
     <span
