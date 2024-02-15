545b9a81cca46a873db775d5cef7c01968da5df1
Amy J. Ko <ajko@uw.edu>
2024-02-15 12:34:05 -0800

    Added open source projects.

--- src/routes/(app)/code/+page.svelte
+++ src/routes/(app)/code/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Block from '$lib/components/Block.svelte';
+    import Link from '$lib/components/Link.svelte';
     import Image from '$lib/components/Thumbnail.svelte';
 </script>
 
@@ -11,19 +12,21 @@
     evolving them.
 </p>
 
-<Block link="https://github.com/wordplaydev" header="Wordplay">
-    <Image
-        slot="image"
-        url="/images/code/wordplay.png"
-        alt="A chat bubble"
-    />&nbsp;is a free platform for creative expression with text and typography;
+<Block link="https://wordplay.dev" header="Wordplay">
+    <Image slot="image" url="/images/code/wordplay.png" alt="A chat bubble" />
+
+    &nbsp;is a free platform for creative expression with text and typography;
     it aspires to radically center accessibility, language, and culture, and to
     support youth learning in families and classrooms. It is also a playground
     for many of my ongoing research questions about power, governance, and
     programming language justice in computing education.
+    <ul>
+        <li>See the <Link to="https://github.com/wordplaydev">source</Link>.</li
+        >
+    </ul>
 </Block>
 
-<Block link="https://github.com/amyjko/bookish" header="Bookish">
+<Block link="https://bookish.press" header="Bookish.press">
     <Image
         slot="image"
         url="/images/code/bookish.png"
@@ -31,9 +34,15 @@
     />&nbsp;is a platform for publishing accessible online multi-chapter books
     for the web. I use it to maintain and publish the textbooks I've written for
     my teaching.
+    <ul>
+        <li
+            >See the <Link to="https://github.com/amyjko/bookish">source</Link
+            >.</li
+        >
+    </ul>
 </Block>
 
-<Block link="https://github.com/reciprocalreviews" header="Reciprocal Reviews">
+<Block link="https://reciprocal.reviews/" header="Reciprocal Reviews">
     <Image
         slot="image"
         url="/images/code/reciprocal.png"
@@ -41,4 +50,11 @@
     />&nbsp;is a platform for enabling peer review volunteering and review
     activity tracking, to enable more sustainable and equitable peer review in
     conferences and journals.
+    <ul>
+        <li
+            >See the <Link to="https://github.com/reciprocalreviews"
+                >source</Link
+            >.</li
+        >
+    </ul>
 </Block>
