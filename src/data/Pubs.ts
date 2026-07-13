f7277345b60e6a34a946f385393c060e68f279c7
Amy J. Ko <ajko@uw.edu>
2026-07-13 08:47:37 -0500

    Added PDF.

--- src/data/Pubs.ts
+++ src/data/Pubs.ts
@@ -64,7 +64,7 @@ export const Publications: Paper[] = [
         pages: null,
         publisher: 'Taylor & Francis',
         doi: 'https://doi.org/10.1080/08993408.2026.2694765',
-        local: null,
+        local: 'Hardin2026Fizzle.pdf',
         contribution:
             'Finds ill-fitting state policies, institutional hurdles, and a lack of computer science teacher preparation resources compound the sustainability of CS pre-service pathways.',
         discoveries: ['TeachingIsHard'],
@@ -3673,3 +3673,50 @@ export const Publications: Paper[] = [
         discoveries: ['CanAskProgramsQuestions', 'DevelopmentNeedsInfo'],
     },
 ];
+import type Annotation from '$lib/models/Annotation';
+import type { DiscoveryID } from './Discoveries';
+
+export type Paper = {
+    id: string;
+    kind:
+        | 'refereed conference paper'
+        | 'refereed workshop paper'
+        | 'journal article'
+        | 'refereed short conference paper'
+        | 'juried conference paper'
+        | 'book chapter'
+        | 'refereed invited article'
+        | 'juried workshop paper'
+        | 'technical report'
+        | 'refereed magazine article'
+        | 'whitepaper'
+        | 'abstract'
+        | 'book';
+    authors: string[];
+    equalfirst?: boolean;
+    deadname?: boolean;
+    year: number;
+    title: string;
+    source: string;
+    discoveries: null | [DiscoveryID, ...DiscoveryID[]];
+    volume?: number;
+    issue?: number;
+    article?: number;
+    pages: string | null;
+    doi: string | null;
+    local: string | null;
+    authorizer?: string | null;
+    publisher: string;
+    contribution: string;
+    slides?: string;
+    blog?: string;
+    acceptrate?: number;
+    annotation?: Annotation;
+    award?: (
+        | 'best paper'
+        | 'diversity + inclusion award'
+        | 'honorable mention'
+        | 'most influential paper'
+        | 'most influential paper honorable mention'
+    )[];
+};
